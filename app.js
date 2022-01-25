da56e85f974c58bdcdf7ecff538ebe2c4fd60d0d
Noah Ukura <nukura@email.neit.edu>
2022-01-24 20:13:35 -0500

    updated port

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
-const port = process.env.port || 3000
+const port = process.env.PORT || 3000
 const app = express()
 
 app.engine('hbs', exphbs.engine({
